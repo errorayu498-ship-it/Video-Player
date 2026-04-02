629d42b98109ae9d4e270a3036e6f6c5f56b394b
errorayu498-ship-it <errorayu498@gmail.com>
2026-04-02 23:49:11 +0500

    Create admin dashboard page

---
+++ admin/dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<link rel="stylesheet" href="../assets/style.css">
+
+</head>
+
+<body>
+
+<h2>Admin Dashboard</h2>
+
+<a href="upload.php">Upload Video</a>
+
+</body>
+
+</html>
