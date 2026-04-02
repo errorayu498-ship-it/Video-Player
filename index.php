0da4fca391fae79d418b5828523bc8b8f15456b7
errorayu498-ship-it <errorayu498@gmail.com>
2026-04-02 23:29:17 +0500

    Update UI elements in index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Universal Video Player</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="container">
+
+<h1 class="title">Universal Video Player</h1>
+
+<form action="player.php" method="GET">
+
+<input type="text" name="url" placeholder="URL" required>
+
+<button type="submit">Watch</button>
+
+</form>
+
+</div>
+
+</body>
+</html>
